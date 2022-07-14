9579fd11c82621bf92d7d033b8ba3d074b07b441
larrysfinolb <larrysfinolb@gmail.com>
2022-07-14 14:16:48 -0400

    pokemon page created

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 let lastSearch = '';
 let names = [];
 
+function loadIndex() {
+	const params = new URLSearchParams(window.location.search);
+	const search = params.get('search');
+
+	if (search != undefined) {
+		lastSearch = search;
+		document.querySelector('#search').value = search;
+		loadPokemonBySearch(search);
+	} else {
+		loadAllPokemon();
+	}
+}
+
 function loadAllPokemon() {
 	document.querySelector('#list').innerHTML = '';
 	axios
@@ -92,60 +105,4 @@ function buttonSearch() {
 		lastSearch = search;
 		loadPokemonBySearch(search);
 	}
-}
-
-// Función para obtener los datos de un pokemon
-function getPokemosn() {
-	const params = new URLSearchParams(window.location.search);
-	const name = params.get('name');
-
-	axios
-		.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
-		.then((response) => {
-			let pokemon = response.data;
-			console.log(pokemon);
-
-			let movesHTML = '';
-			for (const key in pokemon.moves) {
-				movesHTML += `<li>${pokemon.moves[key].move.name.replace(/-/g, ' ')}</li>`;
-			}
-
-			document.querySelector('#pokemon').innerHTML = `
-				<div class="pokemon__info">
-					<p class="pokemon__id">#${pokemon.id}</p>
-					<p class="pokemon__name">${pokemon.name}</p>
-				</div>
-				<div class="pokemon__imgs">
-					<img src="${pokemon.sprites['front_default']}" />
-					<img src="${pokemon.sprites['back_default']}" />
-					<img src="${pokemon.sprites['front_shiny']}" />
-					<img src="${pokemon.sprites['back_shiny']}" />
-				</div>
-				<div class="pokemon__details">
-					<div>
-						<h2 class="pokemon__title">Specie</h2>
-						<ul class="pokemon__grid">
-							<li>Height: ${pokemon.height / 10}M</li>
-							<li>Weight: ${pokemon.weight / 10}Kg</li>
-						</ul>
-					</div>
-					<div>
-						<h2 class="pokemon__title">Stats</h2>
-						<ul class="pokemon__grid">
-							<li>HP: ${pokemon.stats[0].base_stat}</li>
-							<li>Attack: ${pokemon.stats[1].base_stat}</li>
-							<li>Defense: ${pokemon.stats[2].base_stat}</li>
-							<li>Sp. Attack: ${pokemon.stats[3].base_stat}</li>
-							<li>Sp. Defense: ${pokemon.stats[4].base_stat}</li>
-							<li>Speed: ${pokemon.stats[5].base_stat}</li>
-						</ul>
-					</div>
-					<div>
-						<h2 class="pokemon__title">Moves</h2>
-						<ul class="pokemon__list">${movesHTML}</ul>
-					</div>
-				</%&div>
-			`;
-		})
-		.catch((error) => {});
 }
---
+++ js/pokemon.js
@@ -0,0 +1,63 @@
+function getPokemon() {
+	const params = new URLSearchParams(window.location.search);
+	const name = params.get('name');
+
+	axios
+		.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+		.then((response) => {
+			let pokemon = response.data;
+			console.log(pokemon);
+
+			let movesHTML = '';
+			for (const key in pokemon.moves) {
+				movesHTML += `<li>${pokemon.moves[key].move.name.replace(/-/g, ' ')}</li>`;
+			}
+
+			document.querySelector('#pokemon').innerHTML = `
+				<div class="pokemon__info">
+					<p class="pokemon__id">#${pokemon.id}</p>
+					<p class="pokemon__name">${pokemon.name}</p>
+				</div>
+				<div class="pokemon__imgs">
+					<img src="${pokemon.sprites['front_default']}" />
+					<img src="${pokemon.sprites['back_default']}" />
+					<img src="${pokemon.sprites['front_shiny']}" />
+					<img src="${pokemon.sprites['back_shiny']}" />
+				</div>
+				<div class="pokemon__details">
+					<div>
+						<h2 class="pokemon__title">Specie</h2>
+						<ul class="pokemon__grid">
+							<li>Height: ${pokemon.height / 10}M</li>
+							<li>Weight: ${pokemon.weight / 10}Kg</li>
+						</ul>
+					</div>
+					<div>
+						<h2 class="pokemon__title">Stats</h2>
+						<ul class="pokemon__grid">
+							<li>HP: ${pokemon.stats[0].base_stat}</li>
+							<li>Attack: ${pokemon.stats[1].base_stat}</li>
+							<li>Defense: ${pokemon.stats[2].base_stat}</li>
+							<li>Sp. Attack: ${pokemon.stats[3].base_stat}</li>
+							<li>Sp. Defense: ${pokemon.stats[4].base_stat}</li>
+							<li>Speed: ${pokemon.stats[5].base_stat}</li>
+						</ul>
+					</div>
+					<div>
+						<h2 class="pokemon__title">Moves</h2>
+						<ul class="pokemon__list">${movesHTML}</ul>
+					</div>
+				</%&div>
+			`;
+		})
+		.catch((error) => {});
+}
+
+function buttonSearch() {
+	const search = document.querySelector('#search').value;
+	if (search == '') {
+		window.location.href = 'index.html';
+	} else {
+		window.location.href = `index.html?search=${search}`;
+	}
+}
